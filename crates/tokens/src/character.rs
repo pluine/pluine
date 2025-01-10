11dba6d6bc82ac0edeb18b72021d3c2c4469e392
gibbz00 <gabrielhansson00@gmail.com>
2025-01-10 21:59:12 +0100

    refactor(tokens): place character tokens variants in their own modules

--- crates/tokens/src/character.rs
+++ crates/tokens/src/character.rs
@@ -1,35 +1,48 @@
-pub enum Character {
-    Literal(CharacterLiteral),
-    CodePoint(CharacterCodePoint),
-    Name(CharacterName),
+mod character {
+    use crate::*;
+
+    pub enum Character {
+        Literal(CharacterLiteral),
+        CodePoint(CharacterCodePoint),
+        Name(CharacterName),
+    }
 }
 
-// TODO: feature flag unicode support
-/// EBNF-ish: `#\<any char>`
-pub struct CharacterLiteral(char);
-
-// TODO: feature flag unicode support
-/// Unicode point code character representation point.
-/// EBNF: `#\x <HexadecimalDigit>+ | #\X <HexadecimalDigit>+`
-pub struct CharacterCodePoint(char);
-
-pub enum CharacterName {
-    /// EPNF: `#\alarm`
-    Alarm,
-    /// EPNF: `#\backspace`
-    Backspace,
-    /// EPNF: `#\delete`
-    Delete,
-    /// EPNF: `#\escape`
-    Escape,
-    /// EPNF: `#\newline`
-    Newline,
-    /// EPNF: `#\null`
-    Null,
-    /// EPNF: `#\return`
-    Return,
-    /// EPNF: `#\space`
-    Space,
-    /// EPNF: `#\tab`
-    Tab,
+mod literal {
+    // TODO: feature flag unicode support
+    /// EBNF-ish: `#\<any char>`
+    pub struct CharacterLiteral(char);
 }
+pub(crate) use literal::CharacterLiteral;
+
+mod code_point {
+    // TODO: feature flag unicode support
+    /// Unicode point code character representation point.
+    /// EBNF: `#\x <HexadecimalDigit>+ | #\X <HexadecimalDigit>+`
+    pub struct CharacterCodePoint(char);
+}
+pub(crate) use code_point::CharacterCodePoint;
+
+mod name {
+    pub enum CharacterName {
+        /// EPNF: `#\alarm`
+        Alarm,
+        /// EPNF: `#\backspace`
+        Backspace,
+        /// EPNF: `#\delete`
+        Delete,
+        /// EPNF: `#\escape`
+        Escape,
+        /// EPNF: `#\newline`
+        Newline,
+        /// EPNF: `#\null`
+        Null,
+        /// EPNF: `#\return`
+        Return,
+        /// EPNF: `#\space`
+        Space,
+        /// EPNF: `#\tab`
+        Tab,
+    }
+}
+pub(crate) use name::CharacterName;
--- crates/tokens/src/lib.rs
+++ crates/tokens/src/lib.rs
@@ -60,6 +60,7 @@ mod boolean;
 pub(crate) use boolean::Boolean;
 
 mod character;
+pub(crate) use character::*;
 
 mod number;
 pub(crate) use number::*;
