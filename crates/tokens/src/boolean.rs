70384511dfd96bef8b105735a9f6c610b1d29702
gibbz00 <gabrielhansson00@gmail.com>
2025-01-10 22:28:18 +0100

    feat(tokens): begin string declarations

--- crates/tokens/src/boolean.rs
+++ crates/tokens/src/boolean.rs
@@ -1,3 +1,4 @@
 // TEMP: until the tokenizer trait exists to implement directly onto boolean
 /// EBNF: `#t | #T | #true | #TRUE | #f | #F | #false | #FALSE`
+// XXX: #tRuE is also a valid representation
 pub struct Boolean(bool);
--- crates/tokens/src/string/mod.rs
+++ crates/tokens/src/string/mod.rs
@@ -1,1 +1,13 @@
+use crate::*;
 
+pub enum StringElement {
+    Newline(StringNewline)
+    HexEscape(InlineHexEscape),
+}
+
+/// Note: final intraline whitespace
+/// EBNF: `\ <IntralineWhitespace>* <LineEnding> <IntralineWhitespace>*`
+pub struct StringNewline {
+    leading_whitespace: Vec<IntralineWhitespace>,
+    line_ending: LineEnding,
+}
