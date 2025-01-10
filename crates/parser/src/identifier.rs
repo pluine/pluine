f8d18bf9df397e7dbae0d65700e61d9e8a6c39ec
gibbz00 <gabrielhansson00@gmail.com>
2025-01-11 00:45:54 +0100

    feat(parser): model simple identifier

--- crates/parser/src/identifier.rs
+++ crates/parser/src/identifier.rs
@@ -8,12 +8,34 @@ mod core {
     }
 }
 
-mod peculiar {
-    // Invalid
-    // +i and -i and ifnan
-    pub struct PeculiarIdentifier(String);
+mod simple {
+    // TODO: if not(unicode_identifiers):
+    // ascii_initial =  |char| .is_ascii_alphabetic() || is_ascii_non_letter
+    // TODO: if (unicode_identifiers):
+    // char = ascii_initial || (!.is_ascii() && in unicode category)
+    //
+    // Unicode category: Ll, Lt, Lm, Lo, Nd, Nl, No, Pd, Pc, Po, Sc, Sm, Sk, So, or Co categories or is
+    // U+200C or U+200D
+    //
+    //  ASCII Non letter: `! | $ | % | & | * | / | : | < | = | > | ? | @ | ^ | _ | ~`
+    pub struct SimpleInitial(char);
+
+    // TODO: if not(unicode_identifiers):
+    // ascii_subsequent =  |char| .is_ascii_alphanumberic() || is_ascii_non_letter
+    // TODO: if (unicode_identifiers):
+    // char = ascii_initial || (!.is_ascii() && in unicode category)
+    //
+    // Unicode category: Lu, Ll, Lt, Lm, Lo, Mn, Mc, Me, Nd, Nl, No, Pd, Pc, Po, Sc, Sm, Sk, So, or
+    // Co, or is U+200C or U+200DG
+    //
+    // ASCII Non Letter: `! | $ | % | & | * | / | : | < | = | > | ? | @ | ^ | _ | ~ | @ | + | - | .`
+    //
+    // Digit: DecimalDigit
+    pub struct SimpleSubsequent(char);
+
+    pub struct SimpleIdentifier(SimpleInitial, Vec<SimpleSubsequent>);
 }
-pub(crate) use peculiar::PeculiarIdentifier;
+pub(crate) use simple::SimpleIdentifier;
 
 mod vertical {
     use crate::*;
@@ -32,30 +54,11 @@ mod vertical {
 }
 pub(crate) use vertical::VerticalIdentifier;
 
-mod simple {
-    pub struct SimpleIdentifier(String);
+mod peculiar {
+    // Invalid
+    // +i and -i and ifnan
+    pub struct PeculiarIdentifier(String);
+
+    pub struct SignSubsequent();
 }
-pub(crate) use simple::SimpleIdentifier;
-
-mod character {
-    //! "Scheme implementations may permit any additional repertoire of non-ASCII
-    //! Unicode characters ... of Lu, Ll, Lt, Lm, Lo, Mn, Mc, Me, Nd, Nl,
-    //! No, Pd, Pc, Po, Sc, Sm, Sk, So, or Co, or is U+200C or U+200D ... However, it is an error
-    //! for the first character to have a general category of Nd, Mc, or Me."
-    //!
-    //! R7RS short spec - 7.1.1
-    //!
-    //! See top level crate documentation for exceptions to this formulation.
-
-    pub struct IdentifierCharacter(char);
-
-    pub struct FirstIdenfitierCharacter(char);
-
-    impl FirstIdenfitierCharacter {
-        /// Consumes a first identifier candidate character, returning `Some(Self)` it is valid.
-        /// If no additional feature flags are enabled, then thef
-        pub fn from_char(identifier_character: char) -> Option<Self> {
-            todo!()
-        }
-    }
-}
+pub(crate) use peculiar::PeculiarIdentifier;
