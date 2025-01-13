f7f5d4b20ab96129ffd02c38ca81724233e064f1
gibbz00 <gabrielhansson00@gmail.com>
2025-01-13 19:26:49 +0100

    refactor(lexer): move `Span` inside `NumberLiter` variants

--- crates/lex/src/primitive/number/core.rs
+++ crates/lex/src/primitive/number/core.rs
@@ -2,25 +2,32 @@ use crate::*;
 
 #[derive(Debug, PartialEq)]
 pub enum NumberLiteral<'src> {
-    Binary(Number<BinaryDigit>, Span<'src>),
-    Octal(Number<OctalDigit>, Span<'src>),
-    Decimal(Number<DecimalDigit>, Span<'src>),
-    Hexadecimal(Number<HexadecimalDigit>, Span<'src>),
+    Binary(Number<'src, BinaryDigit>),
+    Octal(Number<'src, OctalDigit>),
+    Decimal(Number<'src, DecimalDigit>),
+    Hexadecimal(Number<'src, HexadecimalDigit>),
 }
 
 impl Spanned for NumberLiteral<'_> {
     fn span(&self) -> Span<'_> {
-        *match self {
-            NumberLiteral::Binary(_, span) => span,
-            NumberLiteral::Octal(_, span) => span,
-            NumberLiteral::Decimal(_, span) => span,
-            NumberLiteral::Hexadecimal(_, span) => span,
+        match self {
+            NumberLiteral::Binary(number) => number.span(),
+            NumberLiteral::Octal(number) => number.span(),
+            NumberLiteral::Decimal(number) => number.span(),
+            NumberLiteral::Hexadecimal(number) => number.span(),
         }
     }
 }
 
 #[derive(Debug, PartialEq)]
-pub struct Number<R: Radix> {
+pub struct Number<'src, R: Radix> {
     prefix: Prefix<R>,
     inner: ComplexNumber<R>,
+    span: Span<'src>,
+}
+
+impl<R: Radix> Spanned for Number<'_, R> {
+    fn span(&self) -> Span<'_> {
+        self.span
+    }
 }
