260d4f784f920603941ab4e37699887812eb7353
gibbz00 <gabrielhansson00@gmail.com>
2025-01-13 19:25:03 +0100

    refactor(lexer): make `Boolean` a named struct

--- crates/lex/src/primitive/boolean.rs
+++ crates/lex/src/primitive/boolean.rs
@@ -3,10 +3,13 @@ use crate::*;
 /// EBNF: `#t | #T | #true | #TRUE | #f | #F | #false | #FALSE`
 // XXX: #tRuE is also a valid representation
 #[derive(Debug, PartialEq)]
-pub struct Boolean<'src>(bool, Span<'src>);
+pub struct Boolean<'src> {
+    inner: bool,
+    span: Span<'src>,
+}
 
 impl Spanned for Boolean<'_> {
     fn span(&self) -> Span<'_> {
-        self.1
+        self.span
     }
 }
